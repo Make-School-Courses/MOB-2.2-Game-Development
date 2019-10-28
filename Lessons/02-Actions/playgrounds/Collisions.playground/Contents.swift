69adffe5fd1802c229143057d39929c4a94f0ef3
Adriana <melina.gzz@hotmail.com>
2019-10-28 12:36:50 -0700

    Edit collision playground

--- Lessons/02-Actions/playgrounds/Collisions.playground/Contents.swift
+++ Lessons/02-Actions/playgrounds/Collisions.playground/Contents.swift
@@ -39,9 +39,11 @@ class GameScene: SKScene {
     }
     
     override func update(_ currentTime: TimeInterval) {
-        checkCollisions()
     }
   
+    override func didEvaluateActions() {
+        checkCollisions()
+    }
     
     func collision(with node: SKSpriteNode) {
         print("Hey something just happened!")
